768ee23df2ecdb9dbb8778c853b6ae6a022ae96c
Tom Bloor <tom.bloor@googlemail.com>
2016-03-15 14:42:53 +0000

    Change load_namespaces to load_class for fatpackability

--- Door-Control/lib/LAMM/RFID/DoorControl/Schema.pm
+++ Door-Control/lib/LAMM/RFID/DoorControl/Schema.pm
@@ -2,6 +2,6 @@ package LAMM::RFID::DoorControl::Schema;
 
 use base qw/ DBIx::Class::Schema /;
 
-__PACKAGE__->load_namespaces;
+__PACKAGE__->load_classes({ __PACKAGE__.'::Result' => [ qw(AllowedCard) ] });
 
 1;
