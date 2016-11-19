09f0298910bd9fe6d858608fe6997814c82ed753
Tom Bloor <t.bloor@shadowcat.co.uk>
2016-11-19 13:13:55 +0000

    Modified example python to use lib folder

--- Door-Interface/example.py
+++ Door-Interface/example.py
@@ -1,11 +1,16 @@
 # Example code to open a database and query for a card key using doordb
 
+import sys
+sys.path.append('lib')
+
 from peewee import SqliteDatabase
 import doordb
 
-doordb.init(SqliteDatabase('cards.db'))
-user_id = doordb.get_user(card_key = 'hex card key here')
-if user_id:
-    print "Authenticated user", user_id
-else:
-    print "Unrecognized card key"
+doordb.init(SqliteDatabase('/home/pi/keydb.db'))
+#user_id = doordb.get_user(card_key = 'hex card key here')
+#if user_id:
+#    print "Authenticated user", user_id
+#else:
+#    print "Unrecognized card key"
+for allowed_card in doordb.AllowedCard.select():
+  print "ID: %s ; Card: %s " % ( allowed_card.user_id, allowed_card.card_key )
