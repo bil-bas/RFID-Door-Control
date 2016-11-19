2719cfd98e5e2314f122f2f7a7868ec0fabc15a2
Tom Bloor <t.bloor@shadowcat.co.uk>
2016-11-19 14:18:35 +0000

    Added DoorInterface class to make setup easier

--- Door-Interface/example.py
+++ Door-Interface/example.py
@@ -1,19 +1,22 @@
 # Example code to open a database and query for a card key using doordb
 
+# Hack to allow you to run with virtualenv under sudo
+# https://virtualenv.pypa.io/en/latest/userguide/#using-virtualenv-without-bin-python
+activate_this = 'env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
 import sys
 sys.path.append('lib')
 
-import ConfigParser
-import doordb
+from DoorInterface import DoorInterface
 
-config = ConfigParser.ConfigParser()
-config.read('../config.cfg')
+interface = DoorInterface('../config.cfg')
+interface.dump_cards()
+interface.print_reader_version()
 
-doordb.init(config)
+#doordb.init(config)
 #user_id = doordb.get_user(card_key = 'hex card key here')
 #if user_id:
 #    print "Authenticated user", user_id
 #else:
 #    print "Unrecognized card key"
-for allowed_card in doordb.AllowedCard.select():
-  print "ID: %s ; Card: %s " % ( allowed_card.user_id, allowed_card.card_key )
---
+++ Door-Interface/lib/DoorInterface.py
@@ -0,0 +1,34 @@
+import ConfigParser
+import doordb
+import Adafruit_PN532 as PN532
+
+class DoorInterface:
+
+  db = doordb
+
+  def __init__(self, path):
+    self.config = ConfigParser.ConfigParser()
+    self.config.read(path)
+    self.db.init(self.config)
+    self.card_key = get_hex_array(self.config.get('main', 'key'))
+
+    # Configuration for a Raspberry Pi:
+    CS   = 18 # SSEL
+    MOSI = 23 # MOSI
+    MISO = 24 # MISO
+    SCLK = 25 # SCK
+
+    # Create an instance of the PN532 class.
+    self.rfid_reader = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
+    self.rfid_reader.begin()
+
+  def dump_cards(self):
+    for allowed_card in self.db.AllowedCard.select():
+      print "ID: %s ; Card: %s " % ( allowed_card.user_id, allowed_card.card_key )
+
+  def print_reader_version(self):
+    ic, ver, rev, support = self.rfid_reader.get_firmware_version()
+    print 'Found PN532 with firmware version: {0}.{1}'.format(ver, rev)
+
+def get_hex_array ( string ):
+  return map( ord, string.decode( "hex" ) )
